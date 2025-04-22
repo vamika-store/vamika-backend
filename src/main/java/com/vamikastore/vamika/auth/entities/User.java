50600a226e2df0706e896d6e32b852abd10eb022
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2025-04-22 09:36:20 +0530

    Added User API

---
+++ src/main/java/com/vamikastore/vamika/auth/entities/User.java
@@ -0,0 +1,77 @@
+package com.vamikastore.vamika.auth.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "AUTH_USER_DETAILS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @JsonIgnore
+    private String password;
+
+    private Date createdOn;
+
+    private Date updatedOn;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private String provider;
+
+    private String verificationCode;
+
+    private Boolean enabled= false;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "AUTH_USER_AUTHORITY",joinColumns = @JoinColumn(referencedColumnName = "id"),inverseJoinColumns = @JoinColumn(referencedColumnName = "id"))
+    private List<Authority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setCreatedOn(long timeMillis) {
+        this.createdOn = new Date(timeMillis);
+    }
+    public void setUpdatedOn(long timeMillis) {
+        this.updatedOn = new Date(timeMillis);
+    }
+}
