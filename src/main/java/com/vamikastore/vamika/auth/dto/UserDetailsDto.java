b548a8803ffdb2b929e230fd42d6382c529e6a35
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2025-05-31 11:59:38 +0000

    added auth feature

---
+++ src/main/java/com/vamikastore/vamika/auth/dto/UserDetailsDto.java
@@ -0,0 +1,22 @@
+package com.vamikastore.vamika.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsDto {
+
+    private UUID id;
+    private String firstName;
+    private String lastName ;
+    private String email;
+    private String phoneNumber;
+    private Object authorityList;
+}
